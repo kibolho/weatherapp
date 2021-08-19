aa4fd009e33248717947d95d0437534bf77b7a4f
Abilio Azevedo <abilio10@gmail.com>
2021-08-19 17:48:09 -0300

    feat: default location

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -12,45 +12,47 @@ export default function Home() {
 
   const success = (pos: { coords: any }) => {
     var crd = pos.coords;
+    console.log("crd", crd);
     if (crd.longitude && crd.latitude)
       setCoordinates({ lat: crd.latitude, lon: crd.longitude });
   };
 
   const checkLocationPermissions = (result: any) => {
-    if (result.state === "granted") {
-      navigator.geolocation.getCurrentPosition(success);
-    } else if (result.state === "prompt") {
-      navigator.geolocation.getCurrentPosition(
-        success,
-        () => {
-          throw new Error("Geolocation not founded");
-        },
-        {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0,
-        }
-      );
-    } else if (result.state === "denied") {
-      throw new Error("Geolocation changed");
+    try {
+      if (result.state === "granted") {
+        navigator.geolocation.getCurrentPosition(success);
+      } else if (result.state === "prompt") {
+        navigator.geolocation.getCurrentPosition(
+          success,
+          () => {
+            throw new Error("Geolocation not founded");
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+          }
+        );
+      } else if (result.state === "denied") {
+        throw new Error("Geolocation changed");
+      }
+    } catch (e) {
+      alert("Sorry Not available for your location!");
+      setCoordinates({ lat: "-20.2804461", lon: "-40.3008235" });
     }
   };
   useEffect(() => {
-    try {
-      if (navigator.geolocation) {
-        navigator.permissions
-          .query({ name: "geolocation" })
-          .then(function (result) {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          checkLocationPermissions(result);
+          result.onchange = function (result) {
             checkLocationPermissions(result);
-            result.onchange = function (result) {
-              checkLocationPermissions(result);
-            };
-          });
-      }
-    } catch (e) {
-      alert("Sorry Not available!");
+          };
+        });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
